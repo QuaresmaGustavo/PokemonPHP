a304348624c89925ac95844a881d4844af68a85a
Gustavo Quaresma <quaresmagustavo98@gmail.com>
2023-12-21 10:46:57 -0300

    Alterações gerais

--- funcoes.php
+++ funcoes.php
@@ -1,27 +1,51 @@
 <?php
 
 try{
-    switch($_POST['form']){
-        case $_REQUEST['pesquisa']:
-            $conexao = new PDO('mysql:host=localhost;dbname=bd_pokemon;','root','');
+    $conexao = new PDO('mysql:host=localhost;dbname=bd_pokemon;','root','');
+
+    $funcao = $_GET['funcao'];
+
+    switch($funcao){
+        case 'inserir':
+            
             $nome = $_POST['nome'];
             $tipo = $_POST['tipo'];
             $imagem = $_POST['imagem'];
 
+            $verificar = true;
 
-            $insert = "INSERT INTO `pokemon`(`imagem`,`nome`, `tipo`) VALUES (:imagem,:nome,:tipo)";
-
-            $conectar = $conexao->prepare($insert);
-
-            $conectar->bindParam(':imagem', $imagem);
-            $conectar->bindParam(':nome', $nome);
-            $conectar->bindParam(':tipo', $tipo);
+            $consulta = "SELECT * FROM pokemon";
+            $conectar = $conexao->prepare($consulta);
 
             $conectar->execute();
+
+            while($linha = $conectar->fetch(PDO::FETCH_ASSOC)){
+                if($nome == $linha["nome"]){
+                    $verificar = false;
+                    break;
+                }
+            }
+
+            if($verificar){
+                $insert = "INSERT INTO `pokemon`(`imagem`,`nome`, `tipo`) VALUES (:imagem,:nome,:tipo)";
+
+                $conectar = $conexao->prepare($insert);
+
+                $conectar->bindParam(':imagem', $imagem);
+                $conectar->bindParam(':nome', $nome);
+                $conectar->bindParam(':tipo', $tipo);
+
+                $conectar->execute();
+                echo "Pokemon registrado no banco de dados!!";
+            }else{
+                echo "Pokemon ja registrado no banco de dados!!";
+            }
         break;
 
-        case $_REQUEST['mostrar']:
-            $conexao = new PDO('mysql:host=localhost;dbname=bd_pokemon;','root','');
+        case 'mostrar':
+            header('Access-Control-Allow-Origin:*'); 
+            header('Content-Type: application/json');
+
             $consulta = "SELECT * FROM pokemon";
             $conectar = $conexao->prepare($consulta);
 
@@ -41,16 +65,16 @@ try{
             die(json_encode($resultado));
         break;
 
-        case $_REQUEST['deletar']:
-            $conexao = new PDO('mysql:host=localhost;dbname=bd_pokemon;','root','');
+        case 'deletar':
     
-            $id = $_POST['label'];
+            $id = $_GET['id'];
     
-            $deletar = "DELETE FROM `pokemon` WHERE $id";
-    
+            $deletar = "DELETE FROM `pokemon` WHERE id = $id";
             $conectar = $conexao->prepare($deletar);
-    
             $conectar->execute();
+
+            echo "Pokemon removido do banco de dados!!";
+            
         break;
     }
 }catch(PDOException $error){
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+
+    //CONSUMIR API
     $('#btn').on('click', () => {
         var pesq = $("#pesquisa").val();
 
@@ -8,66 +10,108 @@ $(document).ready(() => {
             method: 'GET',
             dataType: 'json',
             success: function (dados) {
+
                 var img = dados.sprites.other["official-artwork"].front_default;
                 var nome = dados.name;
                 var Nometipo = dados.types[0].type.name;
 
+                //SALVAR POKEMON NO BD
                 $.ajax({
-                    url: 'funcoes.php',
+                    url: 'funcoes.php?funcao=inserir',
                     method: 'POST',
-                    data: { nome: nome, tipo: Nometipo, imagem: img },
-                    success: function () {
-                        $('#btn').val("mostrar");
+                    data: { nome: nome, tipo: Nometipo, imagem: img, },
+                    success: function (response) {
+
+                        $('#mensagem').remove();
+
+                        const mensagem = `<span id="mensagem">${response}</span>`;
+
+                        $('#container').append(mensagem);
+
+                        GerarPokemon();
+                        
                     },
-                    error: function () {
-                        console.log('Erro ao enviar dados: Erro PHP');
+                    error: function (error) {
+
+                        alert('Erro ao enviar dados para o banco de dados ' + error);
+
                     }
                 })
             }, error: function () {
-                alert('Erro ao enviar dados: AJAX API');
+
+                console.log('Erro durante consumo da API');
+
             }
-        })
+        });
+    });
+
+    //BOTÃO PARA GERAR POKEMON DO BD
+    $('#btn_v').on('click', ()=>{
+        GerarPokemon();
     })
 
-    $('#btn_v').on('click',()=>{
+    //FUNÇÃO PARA GERAR POKEMON DO BD
+    function GerarPokemon() {
+        $('.bloco').remove();
         $.ajax({
-            url: 'funcoes.php',
+            url: 'funcoes.php?funcao=mostrar',
             method: 'GET',
-            dataType:'json',
+            dataType: 'json',
             success: function (response) {
-    
-                for(i=0;i < response.length;i++){
+
+                for (i = 0; i < response.length; i++) {
+
                     var pokemon = `<div class="bloco">
-                                        <div class="cabecalho">
-                                            <label class="id" value="${response.id}">${i}</label>
-                                            <button type="button" id="deletar" value="deletar">X</button>
-                                        </div>
-                                        
-                                        <img class="img" src="${response[i].imagem}">
-                                        <div class="informacao">
-                                            <label>nome: ${response[i].nome}</label>
-                                            <label>tipo: ${response[i].tipo}</label>
-                                        </div>
-                                    </div>`;
+                                                <div class="cabecalho">
+                                                    <span class="id">${i + 1}<input type="hidden" value="${response[i].id}"/></span>
+                                                    <button type="button" class="btn_deletar">X</button>
+                                                </div>
+                                                
+                                                <img class="img" src="${response[i].imagem}">
+                                                <div class="informacao">
+                                                    <span>Nome: ${response[i].nome}</span>
+                                                    <span>tipo: ${response[i].tipo}</span>
+                                                </div>
+                                            </div>`;
                     $('#body').append(pokemon);
+
                 }
             },
             error: function (error) {
-                console.log('Erro ao recuperar dados: Erro PHP ', error);
-            }
-        })
-    });
 
-    $('#deletar').on('click',()=>{
+                console.log('Erro ao gerar Pokemon do Banco de dados ' + error);
+
+            }
+        });
+    };
+
+    //DELETAR
+    $(document).on('click', '.btn_deletar', function () {
+
+        var id = $(this).closest('.bloco').find('.id input').val();
+
+        console.log("id " + id);
+
         $.ajax({
-            url: 'funcoes.php',
+            url: 'funcoes.php?funcao=deletar&id=' + id,
             method: 'POST',
             success: function (response) {
-                $('#body').remove(pokemon);
+
+                $('#mensagem').remove();
+
+                $(this).closest('.bloco').remove();
+
+                const mensagem = `<span id="mensagem">${response}</span>`;
+
+                $('#container').append(mensagem);
+
+                GerarPokemon();
             },
             error: function (error) {
-                console.log('Erro ao recuperar dados: Erro PHP ', error);
+
+                console.log('Erro ao deletar pokemon ' + error);
+
             }
-        })
+        });
     });
-})
+});
